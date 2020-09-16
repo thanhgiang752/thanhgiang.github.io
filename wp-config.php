e674f220cfbe8119131f7cc6f0a22c0598535e01
Minh <laminh0166@gmail.com>
2020-09-16 13:16:31 +0700

    change stye and fix full width

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('WP_CACHE', true);
-define( 'WPCACHEHOME', 'C:\xampp1\htdocs\galaxy\wp-content\plugins\wp-super-cache/' );
+define( 'WPCACHEHOME', 'C:\xampp\htdocs\galaxy\wp-content\plugins\wp-super-cache/' );
 define( 'DB_NAME', 'galaxy' );
 
 /** MySQL database username */
